596325b2e4e4e8d2f6d737f7613860da76fcce4f
d-c-c-c <djc3mu@virginia.edu>
2023-11-30 13:49:23 -0500

    dataframe wip

--- vendors.py
+++ vendors.py
@@ -1,4 +1,11 @@
 import requests
+import pandas as pd
+
+recipient = ""
+while len(recipient) <= 2:
+    recipient = input("Input the name or SAM UEI number of the desired company: ")
+    if len(recipient) <= 2:
+        print("Please enter more than two characters.")
 
 payload = {
     "subawards": "false",
@@ -8,8 +15,8 @@ payload = {
     "sort": "Award Amount",
     "filters": {
           "award_type_codes": ["A", "B", "C","D"],
-          "recipient_search_text":["HFHGBMAR2NH6"],
-          "time_period": [{"start_date": "2020-10-01", "end_date": "2023-10-01"}]
+          "recipient_search_text":[recipient],
+          "time_period": [{"start_date": "2021-01-01", "end_date": "2023-10-01"}]
       },
     "fields": [
          "Award ID",
@@ -28,8 +35,36 @@ payload = {
 
 request = requests.post("https://api.usaspending.gov/api/v2/search/spending_by_award", json=payload)
 
-#print(request.json())
 request_dict = request.json()
-print(len(request_dict['results']))
-for i in range(len(request_dict['results'])):
-    print(request_dict['results'][i]["Award Amount"])
+
+"""
+Finds the total amount of contract rewards when given a request dictionary.
+"""
+def get_award_amount(dict):
+    awardTotal = 0
+    for i in range(len(dict['results'])):
+        awardTotal += dict['results'][i]["Award Amount"]
+    return awardTotal
+
+"""
+Prints the awards in descending order when given a request dictionary.
+"""
+def print_awards(dict):
+    if len(dict['results']) == 0:
+        print("No results found.")
+        return
+    else:
+        for i in range(len(dict['results'])):
+            print(f"Award #{int(i+1)}: {dict['results'][i]['Award Amount']:.2f} ")
+        return
+
+print_awards(request_dict)
+print(f'Total awards: {get_award_amount(request_dict):.2f}' )
+
+# Pandas Dataframe work
+req_data = pd.json_normalize(request_dict['results'])
+df = pd.DataFrame(data=req_data)
+df2 = df[['Award ID','Recipient Name','Start Date','End Date','Award Amount']].copy()
+print(df)
+df.to_csv('data.txt', sep="\t", index=False)
+#print(df2)
