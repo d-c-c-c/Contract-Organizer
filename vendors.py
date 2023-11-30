2fbbcc1d32a96978992277f31989190b0fd10cbf
d-c-c-c <djc3mu@virginia.edu>
2023-11-30 14:44:34 -0500

    created main function; generateDataFile

--- vendors.py
+++ vendors.py
@@ -1,12 +1,7 @@
 import requests
 import pandas as pd
 
-recipient = ""
-while len(recipient) <= 2:
-    recipient = input("Input the name or SAM UEI number of the desired company: ")
-    if len(recipient) <= 2:
-        print("Please enter more than two characters.")
-
+#POST request to send to the API
 payload = {
     "subawards": "false",
     "limit": 60,
@@ -15,7 +10,7 @@ payload = {
     "sort": "Award Amount",
     "filters": {
           "award_type_codes": ["A", "B", "C","D"],
-          "recipient_search_text":[recipient],
+          "recipient_search_text":[''], #Empty pending user input
           "time_period": [{"start_date": "2021-01-01", "end_date": "2023-10-01"}]
       },
     "fields": [
@@ -33,9 +28,7 @@ payload = {
     ]
 }
 
-request = requests.post("https://api.usaspending.gov/api/v2/search/spending_by_award", json=payload)
 
-request_dict = request.json()
 
 """
 Finds the total amount of contract rewards when given a request dictionary.
@@ -58,13 +51,51 @@ def print_awards(dict):
             print(f"Award #{int(i+1)}: {dict['results'][i]['Award Amount']:.2f} ")
         return
 
-print_awards(request_dict)
-print(f'Total awards: {get_award_amount(request_dict):.2f}' )
 
-# Pandas Dataframe work
-req_data = pd.json_normalize(request_dict['results'])
-df = pd.DataFrame(data=req_data)
-df2 = df[['Award ID','Recipient Name','Start Date','End Date','Award Amount']].copy()
-print(df)
-df.to_csv('data.txt', sep="\t", index=False)
-#print(df2)
+"""Pandas Dataframe work"""
+
+def generateDataFile(dict):
+    
+    if len(dict) == 0:
+        print("No results found.")
+        return
+    else:
+        try:
+            #Normalizing content from Results column
+            res_data = pd.json_normalize(dict['results'])
+
+            #Creating a Pandas dataframe for the json data and then copying desired columns
+            df = pd.DataFrame(data=res_data)
+            df2 = df[['Award ID','Recipient Name','Start Date','End Date','Award Amount']].copy()
+
+            #Porting data to a csv
+            df2.to_csv('data.txt', sep="\t", index=False)
+            return
+        except KeyError as e:
+            print("ERROR: Company name or SAM UEI not found.")
+
+
+
+
+def main():
+    #Accepting user input for the desired company
+    recipient = ""
+    while len(recipient) <= 2:
+        recipient = input("Input the name or SAM UEI number of the desired company. ")
+        if len(recipient) <= 2:
+            print("Please enter more than two characters.")
+
+    payload['filters']['recipient_search_text'] = [f'{recipient}']
+
+    #TODO: HTTP Error handling
+    request = requests.post("https://api.usaspending.gov/api/v2/search/spending_by_award", json=payload)
+
+    request_dict = request.json()
+
+    
+
+    generateDataFile(request_dict)
+    
+
+if __name__=="__main__": 
+    main() 
